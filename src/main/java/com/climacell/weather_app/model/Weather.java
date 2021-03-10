46f5cecf40ed5ee2131a8299842155423b6f51ae
Rachel <rachel151292@gmail.com>
2021-03-10 19:42:49 +0200

    database connection init

--- src/main/java/com/climacell/weather_app/model/Weather.java
+++ src/main/java/com/climacell/weather_app/model/Weather.java
@@ -2,12 +2,16 @@ package com.climacell.weather_app.model;
 
 import java.sql.Date;
 
+import javax.persistence.Entity;
+
 import lombok.Data;
 
-public @Data class Weather { //TODO @entity
+@Entity
+@Data
+public  class Weather { //TODO @entity
 	
-	private double longitude;
-	private double latitude;
+	private double longitude; //TODO indx ? 
+	private double latitude; //TODO idx?
 	private Date forecastTime; //TODO check if import sql util
 	private double temperature;
 	private double precipitationRate;
