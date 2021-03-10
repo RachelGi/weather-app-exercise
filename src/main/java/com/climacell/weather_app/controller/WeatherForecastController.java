0ef019f4acaa832acebbe9a6ccce420c1fd98857
Rachel <rachel151292@gmail.com>
2021-03-10 22:10:20 +0200

    design db - init

--- src/main/java/com/climacell/weather_app/controller/WeatherForecastController.java
+++ src/main/java/com/climacell/weather_app/controller/WeatherForecastController.java
@@ -1,6 +1,5 @@
 package com.climacell.weather_app.controller;
 
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.climacell.weather_app.model.Location;
 import com.climacell.weather_app.model.Weather;
+import com.climacell.weather_app.repository.LocationRepository;
 import com.climacell.weather_app.repository.WeatherRepository;
 
 @RestController
@@ -19,6 +19,8 @@ public class WeatherForecastController {
 
 	@Autowired
 	private WeatherRepository weatherRepository;
+	@Autowired
+	private LocationRepository locationRepository;
 
 
 	@GetMapping("/")
@@ -27,6 +29,7 @@ public class WeatherForecastController {
 		Location loc = new Location();
 		loc.setLatitude(2.0);
 		loc.setLongitude(3.4);
+		locationRepository.save(loc);
 		weather.setLocation(loc);
 		weather.setTemperature(2.54);
 		weatherRepository.save(weather );
